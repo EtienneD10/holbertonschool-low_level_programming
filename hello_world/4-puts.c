bfeb64dc820290cd0f46408ce43a5b5048d1051b
EtienneD10 <etiennedalmas01@gmail.com>
2022-06-06 09:08:52 -0700

    write a c programm that prints something

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,13 @@
-/#!/bin/bash
 #include <stdio.h>
-int puts("Programming is like building a multilingual puzzle\n")
+/**
+* main - Entry point
+*
+* Description: prints "Programming is like building a multilingual puzzle"
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
