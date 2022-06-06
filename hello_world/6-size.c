680601a49133f717c01d0e16572d181bb682c994
EtienneD10 <etiennedalmas01@gmail.com>
2022-06-06 10:10:02 -0700

    Write a C program that prints the size of various types on the computer it is compiled

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,11 +11,13 @@ int main(void)
 {
 int intType;
 float floatType;
-double doubleType;
+long longType;
 char charType;
+long long int longlongType;
 printf("Size of int: %d bytes\n", sizeof(intType));
 printf("Size of float: %d bytes\n", sizeof(floatType));
-printf("Size of double: %d bytes\n", sizeof(doubleType));
+printf("Size of double: %d bytes\n", sizeof(longType));
 printf("Size of char: %d byte\n", sizeof(charType));
+printf("Size of char: %d byte\n", sizeof(longlongType));
 return (0);
 }
