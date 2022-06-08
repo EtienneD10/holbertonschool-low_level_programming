ad90a0551dfceab2f35d18669b974ca846b4ae8c
EtienneD10 <etiennedalmas01@gmail.com>
2022-06-08 09:38:32 -0700

    Write a program that prints all possible different combinations of two digits.

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -17,19 +17,18 @@ int main(void)
 	{
 		b = a + 1;
 		while (b < 58)
-			{
-				putchar(a);
-				putchar(b);
-
+		{
+			putchar(a);
+			putchar(b);
 			if (a < 56 || b < 57)
-				{
-					putchar(44);
-					putchar(32);
-				}
-			b++;
+			{
+				putchar(44);
+				putchar(32);
 			}
+			b++;
+		}
 		a++;
-	}
+		}
 	putchar(10);
 
 	return (0);
