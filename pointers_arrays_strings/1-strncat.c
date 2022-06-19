1b4db71b1b675f2b64e8be0083b2bd0aa93f993f
EtienneD10 <etiennedalmas01@gmail.com>
2022-06-19 08:38:48 -0700

    Write a function that concatenates two strings.

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -14,9 +14,9 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j, k;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 
-	for (j = 0; dest[j] != '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++)
 
 	if (n > j)
 	{
